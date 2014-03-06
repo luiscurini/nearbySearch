27b95cd552c804c6ba3e727f32274d6fc004c58c
Luis Rodriguez <l.rodriguez@epubli.com>
2014-03-06 14:55:50 +0100

    Adding nearby search

--- nearby.php
+++ nearby.php
@@ -50,20 +50,34 @@ class requestHandler {
 		}
 	}
 
+	//Loads the API_KEY and the urls from the settings file
 	private function getSettings($query) {
 		$settings = parse_ini_file('settings.ini');
 		$this->key = $settings['API_KEY'];
 		$this->url = sprintf($settings['URL_SEARCH_'.$this->type],urlencode($query), $this->key);
 	}
 
+	private function getSettingsNearby($location, $radius, $keyword) {
+		$settings = parse_ini_file('settings.ini');
+		$this->key = $settings['API_KEY'];
+		$this->url = sprintf($settings['URL_SEARCH_'.$this->type],$this->key, $location, urlencode($radius), urlencode($keyword));
+	}
+
+	//Prints the name of the place and its address
 	private function printResults($results) {
 		foreach ($results as $result) {
 			$name = $result['name'];
-			$address = $result['formatted_address'];
-			echo $name ." ".$address ."<br/>";
+			if ($this->type == 'NEARBY') {
+				$address = $result['vicinity'];
+			} else {
+				$address = $result['formatted_address'];
+			}
+			echo $name .", ".$address ."<br/>";
 		}
 	}
 
+
+	//Calls text search from the Google API
 	private function textSearch() {
 		if(isset($this->get['query']) && !empty($this->get['query'])) {  
 		
@@ -90,10 +104,57 @@ class requestHandler {
 		} else {
 			$this->printInvalidStatusMessage($status);
 		}
-	} else {
-		echo '<h2>Usage:</h2><p>Please enter some of the following parameter.</p>
-		<ul><li>query=text+to+search</li></ul>';
+		} else {
+			echo '<h2>Usage:</h2><p>Please enter some of the following parameter.</p>
+			<ul><li>query=text+to+search</li></ul>';
+		}
 	}
+
+	//Calls nearby search. Google API, if location is not defined. Berlin will be the default one
+	private function nearbySearch(){
+		$location = '52.5075419,13.4261419'; //Berlin
+		$radius = '500';
+		if(isset($this->get['query']) && !empty($this->get['query'])) {
+			$keyword = $this->get['query'];
+			
+			//Get optional parameter location
+			if(isset($this->get['location']) && !empty($this->get['location'])) {
+				$location = $this->get['location'];
+			}
+
+			//Get optional parameter radius
+			if(isset($this->get['radius']) && !empty($this->get['radius'])) {
+				$radius = $this->get['radius'];
+			}
+
+			$this->getSettingsNearby($location, $radius, $keyword);
+
+			try {
+				$json = $this->curl($this->url);
+			} catch (Exception $e) {
+		    	echo 'Caught exception: ',  $e->getMessage(), "\n";
+			}
+			
+			try {
+				$json_data = json_decode($json, true);
+			} catch (Exception $e) {
+				echo 'Malformed JSON, check the Google Places API';
+		    	echo 'Caught exception: ',  $e->getMessage(), "\n";
+			}
+
+			$status = $json_data['status'];
+			if($this->checkStatusValid($status)) {
+				$this->printResults($json_data['results']);
+			} else {
+				$this->printInvalidStatusMessage($status);
+			}
+
+		} else {
+			echo '<h2>Usage:</h2><p>Please enter some of the following parameter.</p>
+			<ul><li>keyword=text</li><li>location=latitude,longitude (e.g 52.5075419,13.4261419)</li><li>radius=meters (e.g 100)</li></ul>';
+		}		
+
+
 	}
 
 	public function execute() {
@@ -103,20 +164,19 @@ class requestHandler {
 				$this->textSearch();
 			} elseif ($this->get['type'] == 'nearby') {
 				$this->type = 'NEARBY';
-			} else {
+				$this->nearbySearch();
+			} else { //ignore the type parameter
 				$this->textSearch();
 			}		
-		} else {
+		} else { 
 			$this->textSearch();
 		} 
-
-		
+	
 	}
 }
 	//Using UTF-8
 	header('Content-Type: text/html; charset=UTF-8');
-	error_reporting(E_ALL);
-	ini_set('display_errors', 'On');
+	
 	$handler = new requestHandler($_GET);
 	$handler->execute();
 ?>
